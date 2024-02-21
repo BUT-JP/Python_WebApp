e98c13d3da63de5b743267c51ce6527919a6ab80
BUT-JP <xmpokor25@vutbr.cz>
2024-02-21 08:41:00 +0100

    adding all

--- app.py
+++ app.py
@@ -28,13 +28,13 @@ from PIL import Image
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
-'''
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-'''
+# '''
+# def load_lottieurl(url):
+#     r = requests.get(url)
+#     if r.status_code != 200:
+#         return None
+#     return r.json()
+# '''
 # Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
